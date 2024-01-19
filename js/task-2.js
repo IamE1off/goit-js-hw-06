d9269533077a5b06e62a14de841426cd9a1b800f
IamE1off <132561973+IamE1off@users.noreply.github.com>
2024-01-19 14:08:15 +0100

    update task 2
    private items

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,18 @@
 "use strict";
 class Storage {
+  #items;
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
   getItems() {
-    return this.items;
+    return this.#items;
   }
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
   removeItem(itemToRemove) {
-    const indexToRemove = this.items.indexOf(itemToRemove);
-    this.items.splice(indexToRemove, 1);
+    const indexToRemove = this.#items.indexOf(itemToRemove);
+    this.#items.splice(indexToRemove, 1);
   }
 }
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);

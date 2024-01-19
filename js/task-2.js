f8bd37fa1d5c62fa6f224763117046cd037b9621
IamE1off <132561973+IamE1off@users.noreply.github.com>
2024-01-19 19:53:48 +0100

    update task 2
    error handling

--- js/task-2.js
+++ js/task-2.js
@@ -2,20 +2,39 @@
 class Storage {
   #items;
   constructor(items) {
-    this.#items = items;
+    this.#items = items || [];
   }
+
   getItems() {
     return this.#items;
   }
   addItem(newItem) {
+    if (!newItem) {
+      console.error("Please provide a valid item to add."); //error handling in the addItem method to ensure that the newItem parameter is provided.
+      return;
+    }
     this.#items.push(newItem);
   }
+  // make the removeItem method case-insensitive,  convert both the item to remove and the items in the array to lowercase or uppercase before comparing them.
   removeItem(itemToRemove) {
-    const indexToRemove = this.#items.indexOf(itemToRemove);
+    if (!itemToRemove) {
+      console.error("Please provide a valid item to remove.");
+      return;
+    }
+    const indexToRemove = this.#items.findIndex(
+      (item) => item.toLowerCase() === itemToRemove.toLowerCase()
+    );
     if (indexToRemove !== -1) {
       this.#items.splice(indexToRemove, 1);
     }
   }
+  // removeItem(itemToRemove) {
+  //   const indexToRemove = this.#items.indexOf(itemToRemove);
+  //   // check if the item exists in the Storage
+  //   if (indexToRemove !== -1) {
+  //     this.#items.splice(indexToRemove, 1);
+  //   }
+  // }
 }
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
